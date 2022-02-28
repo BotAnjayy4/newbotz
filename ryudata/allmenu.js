4c9863d10602dd8f5169e6737d635bbee4c0e2e8
BotAnjayy4 <99236727+BotAnjayy4@users.noreply.github.com>
2022-02-28 08:02:24 +0700

    Update allmenu.js

--- ryudata/allmenu.js
+++ ryudata/allmenu.js
@@ -1,6 +1,6 @@
 exports.allmenu = (prefix) => {
 	return`Media Sosial 👾
-Instagram : @Fandyyy._
+kebo:owner
 
 *👤 OWNER MENU*
 ⚘👾 ${prefix}chat [option]
@@ -209,7 +209,6 @@ Instagram : @Fandyyy._
 ⚘👾 ${prefix}report
 ⚘👾 ${prefix}req
 
-Fitur Nya Masih Dikit Jadi
-Subscribe For Update 🤙
+Fitur Ni Doanh Tinggal Pake Jan Bct
 `
 }
